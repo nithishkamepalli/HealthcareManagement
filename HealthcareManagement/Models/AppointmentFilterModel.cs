d763527f3fec502027a67255e0440ac5fd095e58
Sandeep Sagar Panjala <avinashgattineni2000@gmail.com>
2024-04-15 21:06:13 -0500

    completed appointment filter model

---
+++ HealthcareManagement/Models/AppointmentFilterModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthcareManagement.Models
+{
+	public class AppointmentFilterModel
+	{
+        public string? PatientName { get; set; }
+        public string? DoctorName { get; set; }
+        public DateTime? AppointmentDate { get; set; }
+        public int? AppointmentId { get; set; }
+    }
+}
+
