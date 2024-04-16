2d9a3c5c4c1a8eba45fc0aa8cc5324a7976ff781
Sandeep Sagar Panjala <avinashgattineni2000@gmail.com>
2024-04-15 21:05:27 -0500

    completed prescription model

---
+++ HealthcareManagement/Models/PrescriptionModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthcareManagement.Models
+{
+	public class PrescriptionModel
+	{
+        public int? PrescriptionId { get; set; }
+        public int AppointmentId { get; set; }
+        public string MedicationName { get; set; }
+        public string Dosage { get; set; }
+        public string Instructions { get; set; }
+    }
+}
+
