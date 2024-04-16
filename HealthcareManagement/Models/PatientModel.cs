c72dc7f74585abbc93523bafbbb1764ab78f3f1b
Sandeep Sagar Panjala <f724b588@wichita.edu>
2024-04-15 20:48:31 -0500

    completed patient model

---
+++ HealthcareManagement/Models/PatientModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HealthcareManagement.Models
+{
+	public class PatientModel
+	{
+        public int? PatientId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
+
