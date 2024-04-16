b002a69d768836e5a0001df503fae929ccdd1b79
Sandeep Sagar Panjala <f724b588@wichita.edu>
2024-04-15 20:48:55 -0500

    completed doctor model

---
+++ HealthcareManagement/Models/DoctorModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthcareManagement.Models
+{
+	public class DoctorModel
+	{
+        public int? DoctorId { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public string Phone { get; set; }
+    }
+}
+
